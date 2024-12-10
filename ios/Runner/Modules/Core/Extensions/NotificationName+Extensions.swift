3092bfdf1445efafd6c69568422f4e7c3c316606
Joe <joeyoung@guojiang.tv>
2024-12-10 14:03:13 +0800

    add

--- ios/Runner/Modules/Core/Extensions/NotificationName+Extensions.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-extension Notification.Name {
-    static let userCoinsDidChange = Notification.Name("userCoinsDidChange")
-    static let userVIPStatusDidChange = Notification.Name("userVIPStatusDidChange")
-} 
--- ios/Runner/Modules/Features/Profile/Models/HelpItem.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct HelpItem {
-    let title: String
-    let detail: String?
-    let action: () -> Void
-} 
